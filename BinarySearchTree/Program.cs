0f9d892f88c8164f7454bdcc0cbae225b65ffe83
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-17 17:28:23 +0530

    feat: Welcome Message

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("Welcome To Binary Search Tree (BST)!");
+        }
+    }
+}

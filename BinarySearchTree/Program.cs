c2ba75e1c6f6147148681b84a80dd8cc8c816bf3
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-17 18:33:56 +0530

    feat: Added: Create Binary Search Tree and Add Elements

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -8,6 +8,22 @@ namespace BinarySearchTree
         {
             //Welcome Message
             Console.WriteLine("Welcome To Binary Search Tree (BST)!");
+
+            //Call Class
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>();
+
+            //Display Root 
+            Console.WriteLine("Root : 56 ");
+
+            //Add Elements in Tree
+            binarySearch.Insert(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);            
+
+            //Display Result
+            Console.WriteLine("Binary Search Tree Elements ::");            
+            binarySearch.Traverse(binarySearch.root);
+            
         }
     }
 }
